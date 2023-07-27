5fccb70872f47f1ce38b2d6c26959cee42adab1e
Abylay Sagymbayev <abylaydev@gmail.com>
2023-07-27 21:13:07 +0600

    first commit

---
+++ src/main/java/kz/abylay/example/controllers/ExampleController.java
@@ -0,0 +1,22 @@
+package kz.abylay.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ExampleController {
+
+    @GetMapping("/first-page")
+    public String welcomePage() {
+        return "home";
+    }
+
+    @PostMapping("/second")
+    public String secondPage(@RequestParam("fname") String name, @RequestParam("lname") String lastname, @RequestParam("age") Integer age) {
+        System.out.println(name + " " + lastname + " " + age);
+        return "about";
+    }
+
+}
